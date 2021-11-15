e84a03316da3c39d5e4b84ecd5ea9b2b04050436
Bogdan Kovtun <psirex48@gmail.com>
2021-11-16 02:43:09 +0300

    Update license

--- contracts/mocks/AStEthMock.sol
+++ contracts/mocks/AStEthMock.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: agpl-3.0
+// SPDX-FileCopyrightText: 2021 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.10;
 
 import {IAStETH} from "../interfaces/IAStETH.sol";
